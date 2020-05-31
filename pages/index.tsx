142a18bf1fa4ecc3ac51c74cf54ed22d9fc034d1
Adrien Guesnel <adrienguesnel@gmail.com>
2020-05-31 02:33:35 +0200

    create user context with example

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Button from "../components/Button";
+import { UserContext } from "../src/UserContext";
 import { Layout } from "antd";
 
 export default function Index() {
   const { Content } = Layout;
+  const { user, setUser } = useContext(UserContext);
+  const [name, setName] = useState(user.name);
+
+  function changeUsername() {
+    setUser({ name });
+  }
 
   return (
     <Layout>
       <Content style={{ margin: "24px 16px 0" }}>
-        <h1>hello world !</h1>
-        <Button value="My button"></Button>
+        <h1>Hello world {user.name}!</h1>
+        Username :
+        <input
+          placeholder="John"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <Button value="Save" onClick={(e) => changeUsername()}></Button>
       </Content>
     </Layout>
   );
